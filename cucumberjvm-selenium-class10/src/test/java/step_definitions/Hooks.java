b4bcbcde3ee828674c23f7d87091568547027040
machzqcq <pradeep@seleniumframework.com>
2015-01-23 12:48:27 -0500

    Test Data with json using gson

---
+++ cucumberjvm-selenium-class10/src/test/java/step_definitions/Hooks.java
@@ -0,0 +1,59 @@
+package step_definitions;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+public class Hooks{
+    public static WebDriver driver;
+
+    
+    @Before("@web")
+    /**
+     * Delete all cookies at the start of each scenario to avoid
+     * shared state between tests
+     */
+    public void openBrowser() throws MalformedURLException {
+
+        driver = new ChromeDriver();
+   	    System.out.println("Opening Browser....");
+    	driver.manage().deleteAllCookies();
+    }
+
+    @After("@web")
+    /**
+     * Embed a screenshot in test report if test is marked as failed
+     */
+    public void embedScreenshot(Scenario scenario) {
+       
+        if(scenario.isFailed()) {
+        try {
+        	 scenario.write("Current Page URL is " + driver.getCurrentUrl());
+//            byte[] screenshot = getScreenshotAs(OutputType.BYTES);
+            byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        } catch (WebDriverException somePlatformsDontSupportScreenshots) {
+            System.err.println(somePlatformsDontSupportScreenshots.getMessage());
+        }
+        
+        }
+        driver.quit();
+        
+    }
+    
+}
