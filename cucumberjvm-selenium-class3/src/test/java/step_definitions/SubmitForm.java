2d69dc49a2a0729ac1c0d6b47894ed96f72400e6
Pradeep <pradeep@seleniumframework.com>
2021-01-31 12:51:43 -0500

    class3 updates

--- cucumberjvm-selenium-class3/src/test/java/step_definitions/SubmitForm.java
+++ cucumberjvm-selenium-class3/src/test/java/step_definitions/SubmitForm.java
@@ -7,8 +7,10 @@ import org.testng.AssertJUnit;
 
 import com.github.javafaker.Faker;
 
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class SubmitForm {
 	
