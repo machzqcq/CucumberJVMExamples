ca1391ff13bbfee5541b10aa12ccf4746a874adc
Pradeep <pradeep@seleniumframework.com>
2021-01-31 12:30:49 -0500

    updated class2

--- cucumberjvm-selenium-class2/src/test/java/step_definitions/RunCukesTest.java
+++ cucumberjvm-selenium-class2/src/test/java/step_definitions/RunCukesTest.java
@@ -1,12 +1,20 @@
 package step_definitions;
+
 import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import org.w3c.dom.views.AbstractView;
+
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.junit.Cucumber;
+
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "classpath:features",
-		plugin = {"pretty", "html:target/cucumber-html-report","json:cucumber.json"},
-		tags = {}
+		features = {"classpath:features/"},
+		glue = { "step_definitions.Addition", "step_definitions.Hooks", "step_definitions.BrowserCommands"},
+		plugin = {"pretty", "html:target/cucumber-html-report.html","json:target/cucumber.json"}
+		// tags = {}
 		)
 public class RunCukesTest{
+	
 }
