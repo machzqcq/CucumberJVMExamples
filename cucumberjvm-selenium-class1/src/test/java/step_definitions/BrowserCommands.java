63b216ff072546af2dc920fc707bc84afed4d534
Pradeep <pradeep@seleniumframework.com>
2021-02-07 16:32:59 -0500

    trivial update

--- cucumberjvm-selenium-class1/src/test/java/step_definitions/BrowserCommands.java
+++ cucumberjvm-selenium-class1/src/test/java/step_definitions/BrowserCommands.java
@@ -9,6 +9,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
 public class BrowserCommands{
     public WebDriver driver;
     public BrowserCommands()
